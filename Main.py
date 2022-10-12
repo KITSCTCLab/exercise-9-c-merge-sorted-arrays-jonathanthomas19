79ba3cfce608429a7e7f9d4951fca2406086bbfa
JONATHAN THOMAS URK21CS2036 <91773936+jonathanthomas19@users.noreply.github.com>
2022-10-12 09:36:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,54 +1,28 @@
 from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-  x = nums1[0:m]
-  y = nums2[0:n]
-  nums1 = x + y
-  nums1 = merge_sort(nums1)
-  return nums1
-  
-  
-def merge_sort(myList) -> None:
-  if len(myList) > 1:
-        mid = len(myList) // 2
-        left = myList[:mid]
-        right = myList[mid:]
-
-        # Recursive call on each half
-        merge_sort(left)
-        merge_sort(right)
-
-        # Two iterators for traversing the two halves
-        i = 0
-        j = 0
-        
-        # Iterator for the main list
-        k=0
-        
-        
-        while i < len(left) and j < len(right):
-            if left[i] <= right[j]:
-              # The value from the left half has been used
-              myList[k] = left[i]
-              # Move the iterator forward
-              i += 1
-            else:
-                myList[k] = right[j]
-                j += 1
-            # Move to the next slot
-            k += 1
-
-        # For all the remaining values
-        while i < len(left):
-            myList[k] = left[i]
-            i += 1
-            k += 1
-
-        while j < len(right):
-            myList[k]=right[j]
-            j += 1
-            k += 1
-  return myList
+    #Write the code here
+    small_a = nums1[0:m] 
+    small_b = nums2[0:n]
+    i = 0  
+    j = 0
+    k = 0
+    while(i<len(small_a) and j<len(small_b)):
+        if (small_a[i] <= small_b[j]):
+            nums1[k] = small_a[i]
+            i+=1
+        else:
+            nums1[k] = small_b[j]
+            j+=1
+        k+=1
+    while i<len(small_a):
+        nums1[k]=small_a[i]
+        i+=1
+        k+=1
+    while j<len(small_b):
+        nums1[k]=small_b[j]
+        j+=1
+        k+=1 
 
 
 # Do not change the following code
