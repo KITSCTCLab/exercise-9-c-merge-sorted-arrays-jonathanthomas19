577657d681c7b11782aa67ef423e675b943c15c6
JONATHAN THOMAS URK21CS2036 <91773936+jonathanthomas19@users.noreply.github.com>
2022-10-12 09:33:15 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
- 
   x = nums1[0:m]
   y = nums2[0:n]
-  nums1=x+y
+  nums1 = x + y
   nums1 = merge_sort(nums1)
   return nums1
+  
   
 def merge_sort(myList) -> None:
   if len(myList) > 1:
@@ -23,7 +23,8 @@ def merge_sort(myList) -> None:
         j = 0
         
         # Iterator for the main list
-        k = 0
+        k=0
+        
         
         while i < len(left) and j < len(right):
             if left[i] <= right[j]:
